66da6ef8c8fc3b9787e31e1f60441c41e2539b9e
Ytyuuy <coxfiximf@gmail.com>
2025-03-22 11:32:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Dr.Lexi",
   botOwner: 'HypeRaceX', // Your name
-  ownerNumber: '918765147690', //Your number
+  ownerNumber: '919236734780', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
